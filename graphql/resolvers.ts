151bbb131299346250fb04066f58a1bda09895ec
nacuervo <90322795+nacuervo@users.noreply.github.com>
2021-11-20 14:21:07 -0500

    Delete resolvers.ts
    vaciar carpeta main de nuevo

--- graphql/resolvers.ts
+++
@@ -1,5 +0,0 @@
-import { resolversProject } from "../models/project/resolvers";
-import { resolversUser } from "../models/user/resolvers";
-import { resolversAdvance } from "../models/advance/resolvers";
-
-export const resolvers = [resolversProject, resolversUser, resolversAdvance];
